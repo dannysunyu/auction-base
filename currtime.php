3d5388b677a7e92f0dde9745597bb2c7f270a377
Rose Perrone <rose.d.perrone@gmail.com>
2012-08-01 16:29:14 -0700

    Just added bootstrap

---
+++ currtime.php
@@ -0,0 +1,34 @@
+<?php # currtime.php - show current time
+  
+  include ('./sqlitedb.php');
+?>
+
+<html>
+<head>
+<title>AuctionBase</title>
+</head>
+
+<?php 
+  include ('./navbar.html');
+?>
+
+<center>
+<h3> Current Time </h3> 
+
+<?php
+  $query = "select currenttime from Time";
+  
+  try {
+    $result = $db->query($query);
+    $row = $result->fetch();
+    echo "Current time is: ".htmlspecialchars($row["currenttime"]);
+  } catch (PDOException $e) {
+    echo "Current time query failed: " . $e->getMessage();
+  }
+  
+  $db = null;
+?>
+
+</center>
+</html>
+
---
+++ selecttime.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>AuctionBase</title>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"/>
+</head>
+<body>
+	<div class="container">
+		<div class="row-fluid">
+			<div class="span2">
+			</div>
+			<div class="span10">
+<?php 
+  include ('./navbar.html');
+?>
+
+<center>
+<h3> Select a Time </h3> 
+
+  <?php
+    $MM = $_POST["MM"];
+    $dd = $_POST["dd"];
+    $yyyy = $_POST["yyyy"];
+    $HH = $_POST["HH"];
+    $mm = $_POST["mm"];
+    $ss = $_POST["ss"];    
+    $entername = htmlspecialchars($_POST["entername"]);
+    
+    if($_POST["MM"]) {
+      $selectedtime = $yyyy."-".$MM."-".$dd." ".$HH.":".$mm.":".$ss;
+      echo "<center> (Hello, ".$entername.". Previously selected time was: ".$selectedtime.".)</center>";
+    }
+    echo "<br/>";
+    echo "<center>Please select a new time:</center>";
+  ?>
+  <form method="POST" action="selecttime.php">
+  <?php 
+    include ('./timetable.html');
+  ?>
+  </form>
+
+</center>
+</div>
+</div>
+</div>
+</body>
+</html>
